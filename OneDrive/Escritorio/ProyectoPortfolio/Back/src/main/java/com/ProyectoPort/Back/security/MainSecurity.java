96deea00683c37178e19d8a12067b310e0c2ed04
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-02-03 18:32:08 -0300

    Carga en el GitHub

---
+++ OneDrive/Escritorio/ProyectoPortfolio/Back/src/main/java/com/ProyectoPort/Back/security/MainSecurity.java
@@ -0,0 +1,93 @@
+package com.ProyectoPort.Back.security;
+
+import com.ProyectoPort.Back.security.jwt.JwtEntryPoint;
+import com.ProyectoPort.Back.security.jwt.JwtTokenFilter;
+import com.ProyectoPort.Back.security.service.UserDetailsImpl;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity {
+
+    @Autowired
+    UserDetailsImpl userDetailsImpl;
+
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+
+    @Bean("JwtTokenFilter")
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("**").permitAll()
+                .anyRequest().authenticated();
+        
+                //.httpBasic();
+        
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();        
+               
+    }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+        
+        protected  void configure(AuthenticationManagerBuilder auth ) throws Exception{
+            auth.userDetailsService(userDetailsImpl).passwordEncoder((passwordEncoder()));
+        }
+
+      
+    protected void configure(HttpSecurity http) throws Exception {
+        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
+        List<String> list2 = Arrays.asList(new String[]{});
+        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
+        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(list1);
+        corsConfiguration.setAllowedOrigins(list2);
+        corsConfiguration.setAllowedMethods(list3);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(list4);
+
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().configurationSource(request -> corsConfiguration);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+}
---
+++ OneDrive/Escritorio/ProyectoPortfolio/Back/src/main/java/com/ProyectoPort/Back/security/enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.ProyectoPort.Back.security.enums;
+
+public enum RolNombre {
+    ROL_ADMIN, ROL_USER;
+}
